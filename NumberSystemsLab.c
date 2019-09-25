82085811a72846c089c881f38be8f0d2f7134f09
Alvin <31421262+alvinjteng@users.noreply.github.com>
2019-09-25 13:51:13 -0700

    Update NumberSystemsLab.c

--- NumberSystemsLab.c
+++ NumberSystemsLab.c
@@ -1,1 +1,71 @@
+#include <cstdio>
+
+using namespace std;
+
+/*
+ * Use printf and scanf
+ * Use division algorithm to convert base10 and any other number base:
+ * 1. Prompt user to enter and unsigned integer in base 10.
+ * 2. Prompt user a new base (2 < x < 36) to convert received number into the new base.
+ * 3. Construct a string of digits that represents the user's new base.
+ * 4. Output the constructed string.
+ * C
+ * D
+ * E
+ * F
+ * G
+ * H
+ * I
+ * J
+ * K
+ * L
+ * M
+ * N
+ * O
+ * P
+ * Q
+ * R
+ * S
+ * T
+ * U
+ * V
+ * W
+ * X
+ * Y
+ * Z
+ */
+int main() {
+    int x, y, mod;
+    char *result = "0";
+    char *temp = "0";
+    
+    printf("Enter an integer: ");
+    scanf("%d", &x); // User's number is stored into x.
+    
+    printf("Convert %d to base: ", x);
+    scanf("%d", &y); // User's base is stored into y.
+    while ((y<2) || (y>35)) { // Looping until the value of y is between 2 and 35.
+        printf("You may only choose a number between 2 and 35 \n Choose another number to convert %d to base: ", x);
+        scanf("%d", &y);
+    }
+    
+    /* loop?
+     * userNumber(x) % userBase(y) = mod 
+     * take the modulus; if mod is > 9 then change mod to assigned letter
+     * Store mod into a string
+     * Check if userBase is still less than userNewNum
+     * if it is then loop
+     * if it isn't, take that number convert to letter if needed and add to string
+     */
+    
+    while (x>y) {
+        mod = x%y;
+        x = (mod-x)/y;
+        temp = mod;
+        result = temp + result;
+    }
+    
+    return 0;
+}
+
 
